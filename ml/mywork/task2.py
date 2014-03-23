ea84515772bccebdb01bf720f15740f5d4183789
LishuLiu <lishuliu@gmail.com>
2014-03-23 14:24:31 -0500

    initial attempt

--- ml/mywork/task2.py
+++ ml/mywork/task2.py
@@ -8,3 +8,42 @@ Building on task 1, we now want to start to understand the relationships to help
 2. What topics exist within the deals?
 
 """
+import gensim
+import numpy as np
+from gensim import corpora, models, similarities
+from scipy.sparse import * 
+from sklearn.cluster import KMeans  
+
+
+dictionary = corpora.Dictionary.load('deals.dict')
+corpus = corpora.MmCorpus('deals.mm')
+tfidf = models.TfidfModel(corpus)
+corpus_tfidf = tfidf[corpus]
+# convert to scipy sparse matrix
+sm_corpus_tfidf = gensim.matutils.corpus2csc(corpus_tfidf) 
+
+###############################################################################
+# clustering
+
+# k-means clustering
+# the rule of thumb of choosing k is square-root of n
+labeler = KMeans(k=3)
+# note: Kmeans currently only works with CSR type sparse matrix
+labeler.fit(sm_corpus_tfidf.tocsr()) 
+
+# print cluster assignments for each row
+for (row, label) in enumerate(labeler.labels_):
+    print "row %d has label %d"%(row, label)
+
+
+###############################################################################
+# topic modeling
+# Latent Semantic Analysis
+lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)
+corpus_lsi = lsi[corpus_tfidf]
+print lsi.print_topics(2)
+
+# Latent Dirichlet Allocation
+lda = gensim.models.ldamodel.LdaModel(corpus=corpus_tfidf, id2word=dictionary, \
+num_topics=100, update_every=1, chunksize=10000, passes=1)
+lda.print_topics(20)

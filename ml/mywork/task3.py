39421164e942bcad92854e232fbbada73f67f4dc
LishuLiu <lishuliu@gmail.com>
2014-03-23 23:11:43 -0500

    add grid search & performance tuning

--- ml/mywork/task3.py
+++ ml/mywork/task3.py
@@ -14,23 +14,38 @@ Also, provide comments on:
     - How did you test your classifier?
 
 """
-import task1 
-import sys
+
+import task1
+import os
 from sklearn import svm
-#from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.grid_search import GridSearchCV
 
+'''
+###############################################################################
 # step 1: run task1 on good_deals, bad_deals and test_deals
-#os.system('python task1.py ../data/good_deals.txt')
-#os.system('python task1.py ../data/bad_deals.txt')
-#os.system('python task1.py ../data/test_deals.txt')
-
+os.system('python task1.py ../data/good_deals.txt')
+os.system('python task1.py ../data/bad_deals.txt')
+os.system('python task1.py ../data/test_deals.txt')
+'''
+###############################################################################
 # step 2: feature selection
-# select top features from good_deals, extract top features from bad_deals
+# Select top features from good_deals, exclude top features from bad_deals
+# By 'top' I used TF>2 as criteria
+good_features = ['NUM','CODE','URL','$','%','free','pillow','sale',\
+                 'save','get','shipping','off','shop','all','code',\
+                 'buy','link','price']
+bad_deals = ['NUM','URL','deal','link']
+intersect = set(good_features) & set(bad_deals) # set(['URL', 'NUM', 'link'])
+features = list(set(good_features) - set(bad_deals))
+# NUM and URL are kept on purpose, some features are manually removed 
 features = [ 'NUM','CODE','DATE','URL','$','%','free','sale','save',\
              'get','shipping','off','shop','all','code','buy','price']
 
+###############################################################################
 # step 3: construct feature vector
+# Here I used binary as feature value, instead of TF, for simplicity
 def vectorize(filename,label,features,vectors,labels):
     fp = open(filename,'r')
     for line in fp:
@@ -52,34 +67,42 @@ vectorize('../data/good_deals.txt',1,features,train_vector,train_label)
 vectorize('../data/bad_deals.txt',0,features,train_vector,train_label)
 vectorize('../data/test_deals.txt',0,features,test_vector,test_label)
 
-#print train_vector
-#print train_label
-# step 4: train classifier
-#X_train, X_test, y_train, y_test = train_test_split(train_vector, train_label, random_state=0)
-#classifier = svm.SVC(kernel='linear')
-#y_pred = classifier.fit(X_train, y_train).predict(X_test)
-#cm = confusion_matrix(y_test, y_pred)
+###############################################################################
+# step 4: train classifier & step 5: performance tuning
+# grid search on SVC parameters
+C = [2**i for i in range(11)]
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
+                     'C': C},
+                    {'kernel': ['linear'], 'C': C}]
 
-#print(cm)
+clf = GridSearchCV(svm.SVC(C=1), tuned_parameters, cv=5)
+clf.fit(train_vector, train_label)
 
-#pl.matshow(cm)
-#pl.title('Confusion matrix')
-#pl.colorbar()
-#pl.ylabel('True label')
-#pl.xlabel('Predicted label')
-#pl.show()
+print "\nBest parameters set found on training set:"
+print clf.best_estimator_
+print "\nGrid scores on training set:"
+for params, mean_score, scores in clf.grid_scores_:
+    print params,mean_score, scores
 
-classifier = svm.SVC(kernel='linear')    
-classifier.fit(train_vector, train_label)
-pred = classifier.predict(train_vector)
-cm = confusion_matrix(train_label, pred)
-print(cm)
+# performance measure
+# Note that here I use precision score and recall score, other measurement 
+# like ROC can be applied here for more spercific performance tuning. 
+classifier = svm.SVC(clf.best_estimator_)    
+y_true, y_pred = train_label, clf.best_estimator_.predict(train_vector)
+print "\nPerformance on test data:"
+print classification_report(y_true, y_pred)
 
-# step 5: performance tuning
-param_grid = [
-  {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
-  {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
- ]
-
+###############################################################################
+# step 6: test
+# I manually labeled test_deals.txt
+y_test = [0, 1, 0, 0, 0, 1, 0, 0, 0, 1,\
+          0, 0, 1, 0, 1, 0, 0, 0, 1, 0,\
+          1, 0, 0, 0, 0, 1, 0, 0, 0, 0,\
+          0, 0, 1, 0, 0, 1, 0, 0, 0, 1,\
+          1, 1, 0, 1, 0, 0, 0, 0, 0, 1,\
+          0, 0, 1, 0, 0, 1, 0, 0]
+y_true, y_pred = y_test, clf.best_estimator_.predict(test_vector)
+print "\nPerformance on test data:"
+print classification_report(y_true, y_pred)
 
 

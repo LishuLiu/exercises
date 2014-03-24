d57a72d457042b2bcc31cb9e63912422357a036c
LishuLiu <lishuliu@gmail.com>
2014-03-23 19:58:37 -0500

    unit tested and corrected

--- ml/mywork/task1.py
+++ ml/mywork/task1.py
@@ -6,56 +6,66 @@ The deals.txt file is a collection of deal descriptions, separated by a new line
 we want to glean the following insights:
 
 1. What is the most popular term across all the deals?
-A: The most popular term is NUMBER (a catergory of numeric numbers). The following terms are: !, %, free, off.
+A: The most popular term is NUMBER (a catergory of numeric numbers). 
+The followed by: URL, $, %, free.
 
 2. What is the least popular term across all the deals?
 A: There are many terms that only appear once across all the deals. 
 
 3. How many types of guitars are mentioned across all the deals?
-A: The term that occur term guitar are ['flatpicking', 'ibanez', 'art', 'classical', 'exclusive', 'series', 'gypsy', 'taylor', 'gear', 'epiphone', 'mallet', 'bassman', 'boutique', 'rebate', 'music', 'card', 'jazz', 'christian', 'electric', 'big', 'rock', 'slant', 'hd', 'fingerstyle', 'sa-212', 'country', 'martin', 'acoustic', 'flatpick'] 
+A: There are 24 terms co-occur with guitar that seems like types: 
+'flatpicking', 'ibanez', 'art', 'classical', 'exclusive', 'series', 'gypsy', 
+'taylor', 'gear', 'epiphone', 'mallet', 'bassman', 'boutique',
+'jazz', 'christian', 'electric', 'big', 'rock', 'slant', 
+'hd', 'fingerstyle', 'country', 'martin', 'acoustic'
 """
 
 import nltk
 import sys
 import re
 import datetime
+
 from nltk.corpus import stopwords
-from gensim import corpora, models, similarities
+from gensim import corpora
 
 def isDate(datestring):
-    """Match datetime"""
-    datetimeFormat = ['%y-%m-%d','%m/%d/%y','%m/%d','%d-%m-%y','%m-%d','%m-%d-%y',\
-                      '%d/%m/%y','%d.%m.%y'] # more format can be add here
-    Flag = False
+    """Match date and date range"""
+    # date format
+    datetimeFormat = ['%y-%m-%d','%Y-%m-%d','%m/%d/%y','%m/%d/%Y',\
+                      '%m-%d-%y','%m-%d-%Y','%d/%m/%y','%d/%m/%Y',\
+                      '%d.%m.%y','%d.%m.%Y','%d-%m-%y','%d-%m-%Y',\
+                      '%m/%d','%m-%d'] 
     for pattern in datetimeFormat:
         try:
-            if '-' in datestring:                           # date range
-                idx = datestring.index('-')                 # assume that date range will not use mm-dd format  
-                startdate = datestring[:idx]
-                enddate = datestring[idx+1:]
-                datetime.datetime.strptime(startdate, pattern)
-                datetime.datetime.strptime(enddate, pattern)
-            else:
-                datetime.datetime.strptime(datestring, pattern)
+            datetime.datetime.strptime(datestring, pattern)
             return True
-        except ValueError:
-            continue
-    return Flag
-                
-def isCode(codestring):
-    """Match coupon code"""
-    codeFormat = ['\w*\d+\w+']			# more format can be add here
-    Flag = False
-    for pattern in codeFormat:
-        try:
-            if codestring == re.match(pattern,codestring).group():
+        except:
+            pass
+        # assume that date range seperated by '-'
+        if '-' in datestring: 
+            try:                         
+                (start,end) = datestring.split('-')
+                datetime.datetime.strptime(start, pattern)
+                datetime.datetime.strptime(end, pattern)
                 return True
-        except:    
-            continue
-    return Flag
+            except:
+                pass
+    return False
+                
+def isCode(codeString):
+    """Match coupon code"""
+    # assume coupon code must have numbers and letters
+    pattern = '\w*\d+\w+'
+    try:
+        if codeString == re.match(pattern,codeString).group():
+            return True
+    except:    
+        pass
+    return False
         
 def isNum(numString):
     """Match numeric string"""
+    # assumer numbers are digits or doubles
     if '.' in numString:
         idx = numString.index('.')
         return numString[:idx].isdigit() and numString[idx+1:].isdigit()
@@ -64,25 +74,25 @@ def isNum(numString):
             
 def isURL(urlString):
     """Match URL string"""
-    urlFormat = ['\w+\.\w+']			# more format can be add here
-    Flag = False
-    for pattern in urlFormat:
-        try:
-            if re.match(pattern,urlString).group():
-                return True
-        except:    
-            continue
-    return Flag
+    # assume URL must have pattern 'alphanumeric.letters'
+    pattern = '\w+\.[a-zA-Z]+'
+    try:
+        if re.search(pattern,urlString) is not None:
+            return True
+    except:
+        pass    
+    return False
 
 def preprocess(line):
     """Pre-process line into tokens"""
     tk = nltk.word_tokenize(line)
     porter = nltk.PorterStemmer()
-    lmtzr = nltk.stem.wordnet.WordNetLemmatizer()	# needs to download wordnet
-    removelist = stopwords.words('english')		# stopwords should be removed
+    lmtzr = nltk.stem.wordnet.WordNetLemmatizer()
+    # English stopwords should be removed, exclude 'off'
+    removelist = stopwords.words('english')
     removelist.remove('off')
-    removelist.remove('to')
-    symbols = ['*',',','?','-','+','(',')',':','.','&','#','"']		# meaningless symbols shoule be removed
+    # '$' and '%' kept on purpose
+    symbols = ['*',',','?','-','+','(',')',':','.','&','#','"','!'] 
     removelist.extend(symbols)
     
     newtk = []
@@ -90,12 +100,17 @@ def preprocess(line):
     	if token in removelist:
     	    continue
     	else:
-            token = token.lower()		# to lower case
-#           token = porter.stem(token)		# stemming
-            token = lmtzr.lemmatize(token)	# lemmatize
-            while len(token)>1 and token[-1] in symbols:     # remove symbols at the end
+    	    # to lower case
+            token = token.lower()
+            # stemming
+#           token = porter.stem(token)
+            # lemmatization
+            token = lmtzr.lemmatize(token)
+            # remove symbols at the end	
+            while len(token)>1 and token[-1] in symbols:
                 token = token[:-1]
             if token:
+            	# replace special case with pre-defined Catergory
                 if isNum(token):
                     token = 'NUM'
                 elif isDate(token):
@@ -112,70 +127,9 @@ def reverseIndex(tk, docID):
     global ReverseIndex
     global TF
     global guitar
-    def isDate(datestring):
-        """Match datetime"""
-        datetimeFormat = ['%y-%m-%d','%m/%d/%y','%m/%d','%d-%m-%y','%m-%d',\
-                          '%d/%m/%y','%d.%m.%y'] # more format can be add here
-        Flag = False
-        for pattern in datetimeFormat:
-            try:
-                if '-' in datestring:                           # date range
-                    idx = datestring.index('-')                 # assume that date range will not use mm-dd format  
-                    startdate = datestring[:idx]
-                    enddate = datestring[idx+1:]
-                    datetime.datetime.strptime(startdate, pattern)
-                    datetime.datetime.strptime(enddate, pattern)
-                else:
-                    datetime.datetime.strptime(datestring, pattern)
-                return True
-            except ValueError:
-                continue
-        return Flag
-                
-    def isCode(codestring):
-        """Match coupon code"""
-        codeFormat = ['\w*\d+\w+']			# more format can be add here
-        Flag = False
-        for pattern in codeFormat:
-            try:
-                if codestring == re.match(pattern,codestring).group():
-                    return True
-            except:    
-                continue
-        return Flag
-        
-    def isNum(numString):
-        """Match numeric string"""
-        if '.' in numString:
-            idx = numString.index('.')
-            return numString[:idx].isdigit() and numString[idx+1:].isdigit()
-        else:
-            return numString.isdigit()
-            
-    def isURL(urlString):
-        """Match URL string"""
-        urlFormat = ['\w+\.\w+']			# more format can be add here
-        Flag = False
-        for pattern in urlFormat:
-            try:
-                if re.match(pattern,urlString).group():
-                    return True
-            except:    
-                continue
-        return Flag
-                
-            
     looper = 0
     for token in tk:
-        if isNum(token):
-    	    token = 'NUM'
-        elif isDate(token):
-    	    token = 'DATE'
-    	elif isCode(token):
-    	    token = 'CODE'
-    	elif isURL(token):
-    	    token = 'URL'
-    	elif token == 'guitar':
+    	if token == 'guitar':
     	    if looper!=0:
     	        guitar.append(tk[looper-1])
         if TF.has_key(token):
@@ -211,48 +165,58 @@ def readDict(filename,dct):
     fp.close()
     return dct
 
-    
-# construct database: doc, terms
-
 if __name__=='__main__':
+
     filename = sys.argv[1]
+    try:
+        idx = filename.rfind('.')
+        name = filename[:idx]
+        idx = name.rfind('/')
+        name = name[idx+1:]
+    except:
+        pass
     
     DocsTable = {}		# key: docID value: tokens
     TF = {}			# key: term  value: term frequency
-    TF['NUM'] = 0		# special term - catergory NUMBER
-    TF['DATE'] = 0		# special term - catergory DATE
-    TF['CODE'] = 0		# special term - catergory CODE
     ReverseIndex = {}		# key: term  value: list of docID
-    guitar = []			# list guitar types
-    
+    guitar = []			# list guitar types    
     docs = []
-    
 
     fp = open(filename,'r')
     docID = 1
     lastline = ''
     for line in fp:
+        # skip consecutive duplicated lines 
         if line == lastline:
             continue
         tokens = preprocess(line)
         if tokens:
             docs.append(tokens)
-    
-#           DocsTable[docID] = tokens
-#           reverseIndex(tokens,docID)
-#           docID += 1
-#    	lastline = line
+            DocsTable[docID] = tokens
+            reverseIndex(tokens,docID)
+            docID += 1
+     	lastline = line
     fp.close()
+    writeDict(name + '_Docs.csv', DocsTable)
+    writeDict(name + '_ReverseIndex.csv', ReverseIndex)
+    writeDict(name + '_TF.csv', TF)
+    
+    # store dictionary and matrix for task 2
     dictionary = corpora.Dictionary(docs)
-    print dictionary
-    dictionary.save('deals.dict')	# store the dictionary, for future reference
+    dictionary.save('deals.dict')
     corpus = [dictionary.doc2bow(text) for text in docs]
-    corpora.MmCorpus.serialize('deals.mm', corpus) # store to disk, for later use
-    corpora.BleiCorpus.serialize('deals.lda-c', corpus)
+    corpora.MmCorpus.serialize('deals.mm', corpus)
+#    corpora.BleiCorpus.serialize('deals.lda-c', corpus) # for lda-c
     
-    
-#    writeDict('Docs.txt', DocsTable)
-#    writeDict('ReverseIndex.txt', ReverseIndex)
-#    writeDict('TF.txt', TF)
-    print guitar
+    # list guitar types, needs human review
+    print set(guitar)
+    # output
+    # set(['shop', 'flatpicking', 'ibanez', 'art', 'classical', 'exclusive', 
+    # 'series', 'month', 'four', 'gypsy', 'taylor', 'gear', 'epiphone', 'CODE', 
+    # 'mallet', 'bassman', 'boutique', 'rebate', 'NUM', 'music', 'card', 
+    # 'online', 'week', 'product', 'jazz', 'christian', 'electric', 'big', 
+    # 'rock', 'slant', 'link', 'buy', 'hd', 'fingerstyle', 'sa-212', 'money', 
+    # 'country', 'martin', 'arrival', 'sale', 'item', 'learn', 'acoustic', 
+    # 'page', 'flatpick'])
+
    
